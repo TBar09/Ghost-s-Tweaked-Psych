ea15e0adb4ec6fe31e79028617b0a70a37b68c91
TBar09 <131258758+TBar09@users.noreply.github.com>
2025-09-27 20:47:58 -0500

    Update setup-windows.bat

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,15 +3,17 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
+haxelib set lime 8.0.2
+haxelib set openfl 9.2.2
+haxelib set flixel 5.6.2
+haxelib set flixel-addons 3.2.2
+haxelib set flixel-ui 2.5.0
+haxelib install flixel-tools 
+haxelib install hxWindowColorMode 
 haxelib install hxCodec
 haxelib install tjson
+haxelib git hscript-improved-dev https://github.com/CodenameCrew/hscript-improved.git codename-dev
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
